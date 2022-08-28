dbe1074d673cee28183d6a8c7a70e6231d944829
donyemachi <donyemachi@gmail.com>
2022-08-28 15:48:02 +0300

    A script that converts the users' details to XML
    Resolve #4

---
+++ mac-import.py
@@ -0,0 +1,58 @@
+import yaml
+from rich import print
+from jinja2 import Environment, FileSystemLoader
+import yaml
+from jinja2 import Template
+
+
+from jinja2 import Environment, FileSystemLoader
+import pandas as pd
+from macaddress import EUI48, EUI64, MAC, OUI
+import macaddress
+import copy
+from datetime import datetime
+import numpy as np
+from jinja2 import Template
+from jinja2 import Template, StrictUndefined
+import yaml
+
+
+df1 = pd.read_excel('mac-list.xlsx')
+df2 = copy.deepcopy(df1)
+
+def mmac(mm):
+    return macaddress.MAC(str(mm))
+
+
+
+df2['user_mac'] = df2.apply(lambda row: mmac(row['EthernetMacAddress']), axis = 1)
+
+df3 = df2[["AddressName","user_mac"]] 
+
+
+df4 = df3.replace(r'^\s*$', 'unknown-user', regex=True)
+dict2 = df4.to_dict(orient="list")
+
+ss = list(dict2.values())[0]
+sss = list(dict2.values())[1]
+
+c = {str(new_key1):str(new_value1) for (new_key1,new_value1) in zip(ss,sss)}
+
+
+cas = {'users':c}
+
+
+#read your jinja template file
+with open("template.j2") as file:
+    template = Template(file.read())
+    
+   
+
+#use jinja to render your configuration
+aa = template.render(cas)
+
+with open("wired-mac.xml", mode="w", encoding="utf-8") as results:
+    results.write(aa)
+
+#print(aa)
+#print(variables)
